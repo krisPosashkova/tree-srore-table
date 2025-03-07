fe326868bb7fa5edfddecc535ab5ccfd48fb8391
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2025-03-07 13:15:07 +0400

    feat(global): add global depends

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ModuleRegistry } from 'ag-grid-community';
+import { AllEnterpriseModule, LicenseManager } from "ag-grid-enterprise";
+import { KEY_AG } from '@/constants'
+
+LicenseManager.setLicenseKey(KEY_AG);
+
+ModuleRegistry.registerModules([
+  AllEnterpriseModule
+]);
 
 createApp(App).mount('#app')
